5f1607d71fa7fecf2ae8e7918d1d3d3cbed95f67
renoycb <48327628+renoycb@users.noreply.github.com>
2024-02-07 17:35:27 +0530

    category fix

--- medusa-config.js
+++ medusa-config.js
@@ -35,8 +35,9 @@ const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
   `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
+   `medusa-payment-manual`,
   `medusa-plugin-wishlist`,
+
   {
     resolve: `@medusajs/file-local`,
     options: {
@@ -84,7 +85,7 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  redis_url: REDIS_URL
+  //redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
