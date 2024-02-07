08bdd8856d5ddf2b6901c9f7b099472061fb75be
renoycb <48327628+renoycb@users.noreply.github.com>
2024-02-07 19:03:13 +0530

    image fixed

--- medusa-config.js
+++ medusa-config.js
@@ -37,13 +37,28 @@ const plugins = [
   `medusa-fulfillment-manual`,
    `medusa-payment-manual`,
   `medusa-plugin-wishlist`,
-
   {
-    resolve: `@medusajs/file-local`,
+    resolve: `medusa-file-s3`,
     options: {
-      upload_dir: "uploads",
+        s3_url: process.env.S3_URL,
+        bucket: process.env.S3_BUCKET,
+        region: process.env.S3_REGION,
+        access_key_id: process.env.S3_ACCESS_KEY_ID,
+        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        cache_control: process.env.S3_CACHE_CONTROL,
+        // optional
+        download_file_duration:
+          process.env.S3_DOWNLOAD_FILE_DURATION,
+        prefix: process.env.S3_PREFIX,
     },
   },
+
+  // {
+  //   resolve: `@medusajs/file-local`,
+  //   options: {
+  //     upload_dir: "uploads",
+  //   },
+  // },
   {
     resolve: 'medusa-plugin-category-images',
     options: {
